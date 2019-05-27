50bab9dc03592b12e47b1da71dc28d6f7be41e3c
Ivan Nepomnyashchikh <nepomnyi@oregonstate.edu>
2019-05-27 16:24:23 -0700

    Comments are added to parameters.py.

--- stin/parameters.py
+++ stin/parameters.py
@@ -1,8 +1,8 @@
-γ = 1.3
-C_0 = 1.1
-v_s = 0.2
-a = 500
-ρ_L = 1000
-g = 9.81
-f = 0.03
-D = 0.002 # m^2 - for 1" (2.5cm) annulus but without annulus
+γ = 1.3 # coefficient of adiabatic process - 5th equation of the system
+C_0 = 1.1 # distribution coefficient - 6th equation of the system
+v_s = 0.2 # slip velocity - 6th equation of the system
+a = 500 # acoustic velocity - 5th equation of the system
+ρ_L = 1000 # density of the liquid phase - 3rd equation of the system
+g = 9.81 # acceleration of gravity - 3rd equation of the system
+f = 0.03 # friction factor - 3rd equation of the system
+D = 0.002 # m^2 - for 1" (2.5cm) annulus (as if there is one) - 3rd equation of the system
